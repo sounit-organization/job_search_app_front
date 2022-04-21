d91b70af5eab10607040647d91efaddaf7dae1c7
SOunit <workbookwork7@gmail.com>
2022-04-21 16:43:19 -0700

    add fav icon to header

--- src/components/molecules/nav-items.tsx
+++ src/components/molecules/nav-items.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { INavItem } from "../../domain/nav-item";
 import NavItem from "../atoms/nav-item";
+import ShowFavIcon from "../atoms/show-fav-icon";
 // import ShowFavIcon from "../atoms/show-fav-icon";
 
 interface Props {
@@ -13,9 +14,7 @@ const NavItems: FC<Props> = (props) => {
       {props.items.map((item) => (
         <NavItem key={item.id} item={item} />
       ))}
-
-      {/* first fix redux then uncomment this */}
-      {/* <ShowFavIcon /> */}
+      <ShowFavIcon />
     </>
   );
 };
