0e88b26d72e2873b34108514eda6a3656d34dc36
SOunit <workbookwork7@gmail.com>
2022-04-21 17:38:22 -0700

    fix test using provider

---
+++ test/test-utils/testing-library-utils.tsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "../../src/services/re-ducks/store";
+
+const reduxStore = configureStore();
+
+const renderWithWrapper = (ui: any, options?: any) => {
+  const Wrapper = ({ children }: any) => {
+    return (
+      <Provider store={reduxStore}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </Provider>
+    );
+  };
+
+  return render(ui, { wrapper: Wrapper, ...options });
+};
+
+export * from "@testing-library/react";
+export { renderWithWrapper };
