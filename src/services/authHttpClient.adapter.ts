e76a5133b073242f62f80f956655a7c8e39478cb
SOunit <workbookwork7@gmail.com>
2022-07-06 15:11:04 -0700

    add update auth state after signup

--- src/services/authHttpClient.adapter.ts
+++ src/services/authHttpClient.adapter.ts
@@ -1,4 +1,5 @@
 import { API_URL } from "../constants/constants";
+import { User } from "../domain/User";
 import httpClientAdapter from "./httpClient.adapter";
 
 type SignUpData = {
@@ -8,7 +9,9 @@ type SignUpData = {
   password: string;
 };
 
-export const signUp = (signUpData: SignUpData) => {
+type SignupResponse = { user: User; token: string };
+
+export const signUp = (signUpData: SignUpData): Promise<SignupResponse> => {
   return httpClientAdapter
     .post(`${API_URL}/auth/signup`!, signUpData)
     .then((res) => res.data);
--- src/services/redux/store.ts
+++ src/services/redux/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authSlice";
 import favoritesReducer from "./favoriteSlice";
 
 export const store = configureStore({
   reducer: {
+    auth: authReducer,
     favorites: favoritesReducer,
   },
 });
