15adefa7725448f61b750f999b2d584d2df7fb9e
SOunit <workbookwork7@gmail.com>
2022-07-06 14:36:32 -0700

    fix test

--- test/test-utils/testing-library-utils.tsx
+++ test/test-utils/testing-library-utils.tsx
@@ -1,21 +1,19 @@
-// import { render } from "@testing-library/react";
-// import { Provider } from "react-redux";
-// import { BrowserRouter } from "react-router-dom";
-// import { configureStore } from "../../src/services/redux/store";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "../../src/services/redux/store";
 
-// const reduxStore = configureStore();
+const renderWithWrapper = (ui: any, options?: any) => {
+  const Wrapper = ({ children }: any) => {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </Provider>
+    );
+  };
 
-// const renderWithWrapper = (ui: any, options?: any) => {
-//   const Wrapper = ({ children }: any) => {
-//     return (
-//       <Provider store={reduxStore}>
-//         <BrowserRouter>{children}</BrowserRouter>
-//       </Provider>
-//     );
-//   };
+  return render(ui, { wrapper: Wrapper, ...options });
+};
 
-//   return render(ui, { wrapper: Wrapper, ...options });
-// };
-
-// export * from "@testing-library/react";
-// export { renderWithWrapper };
+export * from "@testing-library/react";
+export { renderWithWrapper };
