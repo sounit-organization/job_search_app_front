d0dfcbd0a6aef80a072db9c8ac87044b08481e20
SOunit <workbookwork7@gmail.com>
2022-07-06 20:26:36 -0700

    fix test

--- test/presentation/organisms/header.spec.tsx
+++ test/presentation/organisms/header.spec.tsx
@@ -8,21 +8,21 @@ it("fake test", () => {
   expect("").toBe("");
 });
 
-describe("header navigate user correctly", () => {
-  // it("clicks logo and navigate to top page", async () => {
-  //   renderWithWrapper(<App />);
-  //   const headerLogo = screen.getByRole("heading", { name: /job search/i });
-  //   const addSkillLink = screen.getByRole("link", { name: /add skill/i });
-  //   userEvent.click(addSkillLink);
-  //   userEvent.click(headerLogo);
-  //   const findJobsButton = screen.getByRole("button", { name: /find jobs/i });
-  //   expect(findJobsButton).toBeInTheDocument();
-  // });
-  // it("clicks add skill and navigate to add skill page", async () => {
-  //   renderWithWrapper(<App />);
-  //   const addSkillLink = screen.getByRole("link", { name: /add skill/i });
-  //   userEvent.click(addSkillLink);
-  //   const addSkillButton = screen.getByRole("button", { name: /add skill/i });
-  //   expect(addSkillButton).toBeInTheDocument();
-  // });
-});
+// describe("header navigate user correctly", () => {
+// it("clicks logo and navigate to top page", async () => {
+//   renderWithWrapper(<App />);
+//   const headerLogo = screen.getByRole("heading", { name: /job search/i });
+//   const addSkillLink = screen.getByRole("link", { name: /add skill/i });
+//   userEvent.click(addSkillLink);
+//   userEvent.click(headerLogo);
+//   const findJobsButton = screen.getByRole("button", { name: /find jobs/i });
+//   expect(findJobsButton).toBeInTheDocument();
+// });
+// it("clicks add skill and navigate to add skill page", async () => {
+//   renderWithWrapper(<App />);
+//   const addSkillLink = screen.getByRole("link", { name: /add skill/i });
+//   userEvent.click(addSkillLink);
+//   const addSkillButton = screen.getByRole("button", { name: /add skill/i });
+//   expect(addSkillButton).toBeInTheDocument();
+// });
+// });
