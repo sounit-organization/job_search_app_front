b87f75d5a6ae15bda35583df1ecb87e7ca5181fd
SOunit <workbookwork7@gmail.com>
2022-07-13 15:07:20 -0700

    fix mock code

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -7,23 +7,20 @@
 // import { jobsUrl } from "../src/presentation/pages/JobList";
 // import { jobDataList, skillsDataList } from "./data";
 
-// export const handlers = [
-//   rest.post(`${API_URL}/skills`, (req, res, ctx) => {
-//     const body = req.body;
-//     const { skill } = body as any;
-//     return res(ctx.json({ skill }));
-//   }),
-
-//   rest.get(fetchSkillsUrl, (req, res, ctx) => {
-//     return res(ctx.json(skillsDataList));
-//   }),
-
-//   rest.post(createJobUrl, (req, res, ctx) => {
-//     const { title } = req.body as any;
-//     return res(ctx.status(200), ctx.json({ title }));
-//   }),
-
-//   rest.get(jobsUrl, (req, res, ctx) => {
-//     return res(ctx.json(jobDataList));
-//   }),
-// ];
+export const handlers = [
+  //   rest.post(`${API_URL}/skills`, (req, res, ctx) => {
+  //     const body = req.body;
+  //     const { skill } = body as any;
+  //     return res(ctx.json({ skill }));
+  //   }),
+  //   rest.get(fetchSkillsUrl, (req, res, ctx) => {
+  //     return res(ctx.json(skillsDataList));
+  //   }),
+  //   rest.post(createJobUrl, (req, res, ctx) => {
+  //     const { title } = req.body as any;
+  //     return res(ctx.status(200), ctx.json({ title }));
+  //   }),
+  //   rest.get(jobsUrl, (req, res, ctx) => {
+  //     return res(ctx.json(jobDataList));
+  //   }),
+];
