8d7b051ad21a12a01e9cc9575589379d1e313770
SOunit <workbookwork7@gmail.com>
2022-07-13 15:04:14 -0700

    comment out mock code

--- mocks/data.ts
+++ mocks/data.ts
@@ -1,38 +1,38 @@
-import { IJob } from "../src/domain/Job";
-import { ISkill } from "../src/domain/Skill";
+// import { IJob } from "../src/domain/Job";
+// import { ISkill } from "../src/domain/Skill";
 
-export const skillsDataList: { skills: ISkill[] } = {
-  skills: [
-    { _id: "s1", title: "react" },
-    { _id: "s2", title: "node" },
-  ],
-};
+// export const skillsDataList: { skills: ISkill[] } = {
+//   skills: [
+//     { _id: "s1", title: "react" },
+//     { _id: "s2", title: "node" },
+//   ],
+// };
 
-export const jobDataList: { jobs: IJob[] } = {
-  jobs: [
-    {
-      _id: "j1",
-      title: "test title1",
-      companyName: "test company name1",
-      city: "test city1",
-      payment: 1.11,
-      description: "test description1",
-      skills: [
-        { _id: "s1", title: "React" },
-        { _id: "s2", title: "Node" },
-      ],
-    },
-    {
-      _id: "j2",
-      title: "test title2",
-      companyName: "test company name2",
-      city: "test city2",
-      payment: 2.22,
-      description: "test description2",
-      skills: [
-        { _id: "s3", title: "PHP" },
-        { _id: "s4", title: "Docker" },
-      ],
-    },
-  ],
-};
+// export const jobDataList: { jobs: IJob[] } = {
+//   jobs: [
+//     {
+//       _id: "j1",
+//       title: "test title1",
+//       companyName: "test company name1",
+//       city: "test city1",
+//       payment: 1.11,
+//       description: "test description1",
+//       skills: [
+//         { _id: "s1", title: "React" },
+//         { _id: "s2", title: "Node" },
+//       ],
+//     },
+//     {
+//       _id: "j2",
+//       title: "test title2",
+//       companyName: "test company name2",
+//       city: "test city2",
+//       payment: 2.22,
+//       description: "test description2",
+//       skills: [
+//         { _id: "s3", title: "PHP" },
+//         { _id: "s4", title: "Docker" },
+//       ],
+//     },
+//   ],
+// };
