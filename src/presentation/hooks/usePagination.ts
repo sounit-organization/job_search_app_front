a4d870ed97d2429fbe215a9fb439573336ad82b7
SOunit <workbookwork7@gmail.com>
2022-07-21 13:49:35 -0700

    add query string to keep page

--- src/presentation/hooks/usePagination.ts
+++ src/presentation/hooks/usePagination.ts
@@ -1,24 +1,39 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import { Pagination } from "../../services/jobHttpClient.adapter";
 
 export const usePagination = (
   initialPagination: Pagination,
   ITEMS_PER_PAGE: number
 ) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const paramPage = searchParams.get("page");
+
   const [pagination, setPagination] = useState(initialPagination);
   const [page, setPage] = useState(1);
   const { limit } = pagination;
 
-  const onPageChange = (_: any, page: number) => {
-    const skip = (page - 1) * ITEMS_PER_PAGE;
-    setPage(page);
-    setPagination((prevState) => ({ ...prevState, skip, limit }));
-  };
+  const onPageChange = useCallback(
+    (_: any, page: number) => {
+      const skip = (page - 1) * ITEMS_PER_PAGE;
+      setPage(page);
+      setPagination((prevState) => ({ ...prevState, skip, limit }));
+    },
+    [ITEMS_PER_PAGE, limit]
+  );
 
   const initPagination = () => {
     setPage(1);
     setPagination(initialPagination);
   };
 
-  return { pagination, onPageChange, initPagination, page };
+  useEffect(() => {
+    if (paramPage) {
+      onPageChange(null, +paramPage);
+    } else {
+      setSearchParams({ ...searchParams, page: "1" });
+    }
+  }, [paramPage, onPageChange, page, setSearchParams, searchParams]);
+
+  return { pagination, onPageChange, initPagination, page, setSearchParams };
 };
